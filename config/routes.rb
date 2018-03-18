6d88ec9aa841d1b5e796234abf280a969fc08843
EC2 Default User <ec2-user@ip-172-31-44-63.us-east-2.compute.internal>
2018-03-18 22:26:38 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'toppages/index'
+
+  root to: 'toppages#index'
   root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
   resources :tasks
+  resources :users, only: [:index, :show, :new, :create]
 end
